20dc3a5bd6ce0e59f1bc497ee0679179d54890a6
Orçun Tuna <turboindirblog@gmail.com>
2020-08-23 20:46:30 +0300

    status circle tooltip

--- src/components/StatusCircle.js
+++ src/components/StatusCircle.js
@@ -1,18 +1,28 @@
 import React from 'react'
 import PropTypes from 'prop-types'
+import { Tooltip } from 'antd'
 import cn from 'classnames'
 import styles from '../../styles/StatusCircle.module.css'
 
+const typeNames = {
+  online: 'Online',
+  idle: 'Idle',
+  doNotDisturb: 'Do Not Disturb',
+  offline: 'Offline',
+}
+
 function StatusCircle({ type }) {
   return (
-    <div
-      className={cn(styles.circle, {
-        [styles.online]: type === 'online',
-        [styles.idle]: type === 'idle',
-        [styles.doNotDisturb]: type === 'doNotDisturb',
-        [styles.offline]: type === 'offline',
-      })}
-    />
+    <Tooltip placement="top" title={<span>{typeNames[type]}</span>} color={'#000'} mouseEnterDelay={0}>
+      <div
+        className={cn(styles.circle, {
+          [styles.online]: type === 'online',
+          [styles.idle]: type === 'idle',
+          [styles.doNotDisturb]: type === 'doNotDisturb',
+          [styles.offline]: type === 'offline',
+        })}
+      />
+    </Tooltip>
   )
 }
 
