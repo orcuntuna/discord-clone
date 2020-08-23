f4e56914a875313d0797bcadb01163c161b7bef1
Orçun Tuna <turboindirblog@gmail.com>
2020-08-23 15:17:12 +0300

    init project

---
+++ src/components/Channels.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import styles from '../../styles/Channels.module.css'
+
+function Channels() {
+  return (
+    <div className={styles.container}>
+      <p>hello</p>
+    </div>
+  )
+}
+
+export default Channels
